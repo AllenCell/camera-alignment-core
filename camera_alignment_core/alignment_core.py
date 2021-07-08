8e157d291e15ed4249b84d74c79052b76b87fc23
Aditya Nath <aditya.nath@alleninstitute.org>
2021-07-08 14:37:43 -0700

    Added get_channel_name_to_index_map function
    Context: To properly process files needing to be aligned, we need to make sure that we know what channels exist and where each of those channels are in the file

--- camera_alignment_core/alignment_core.py
+++ camera_alignment_core/alignment_core.py
@@ -25,8 +25,26 @@ class AlignmentCore:
     ) -> numpy.typing.ArrayLike:
         raise NotImplementedError("align_image")
 
-    def get_channel_name_to_index_map(self):
-        raise NotImplementedError("align_image")
+    def get_channel_name_to_index_map(self,im_path):
+        # Maps all channels in a split image to an index of where the channel is in the image
+        # Format is {"Raw channel_name" : Channel index}
+
+        im = AICSImage(im_path)
+        channels = im.channel_names
+        channel_info_dict = dict()
+        for channel in channels:
+            if channel in ['Bright_2', 'TL_100x']:
+                channel_info_dict.update({'Raw brightfield': channels.index(channel)})
+            elif channel in ['EGFP']:
+                channel_info_dict.update({'Raw 488nm': channels.index(channel)})
+            elif channel in ['CMDRP']:
+                channel_info_dict.update({'Raw 638nm': channels.index(channel)})
+            elif channel in ['H3342']:
+                channel_info_dict.update({'Raw 405nm': channels.index(channel)})
+            elif channel in ['TaRFP']:
+                channel_info_dict.update({'Raw 561nm': channels.index(channel)})
+
+        return channel_info_dict
 
     def crop(self):
         raise NotImplementedError("align_image")
