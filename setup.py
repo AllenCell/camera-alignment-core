3963bcc37ca43935d8f98ecb87bf68b2980f2ff1
Gabe Medrash <gabeme@alleninstitute.org>
2021-09-09 11:09:39 -0700

    Bump version: 0.1.0 → 1.0.0.dev0

--- setup.py
+++ setup.py
@@ -87,6 +87,6 @@ setup(
     url="https://github.com/aics-int/camera_alignment_core",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.1.0",
+    version="1.0.0.dev0",
     zip_safe=False,
 )
