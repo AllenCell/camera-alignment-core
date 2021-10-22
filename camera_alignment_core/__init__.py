53be67ec06c2fec530a2d9bf8acc205556538cd2
Gabe Medrash <gabeme@alleninstitute.org>
2021-10-22 16:45:14 -0700

    Bump version: 1.0.0.dev0 → 1.0.0.dev1

--- camera_alignment_core/__init__.py
+++ camera_alignment_core/__init__.py
@@ -2,7 +2,7 @@ __author__ = "AICS"
 
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "1.0.0.dev0"
+__version__ = "1.0.0.dev1"
 
 
 def get_module_version():
