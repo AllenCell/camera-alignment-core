4b8b3edfca58ad00c4231b8d656576647b579029
Filip Sluzewski <mfs4rd@virginia.edu>
2021-10-29 15:09:31 -0700

    add comments and fixing constants unused variable

--- camera_alignment_core/alignment_utils/alignment_qc.py
+++ camera_alignment_core/alignment_utils/alignment_qc.py
@@ -21,9 +21,18 @@ class AlignmentQC:
         reference_seg: NDArray[np.uint16] = None,
         moving_seg: NDArray[np.uint16] = None,
         ref_mov_coor_dict: Dict[Tuple[int, int], Tuple[int, int]] = None,
-        rev_coor_dict: Dict[Tuple[int, int], Tuple[int, int]] = None,
         tform: tf.SimilarityTransform = None,
     ):
+        """
+        Initialization of all variables needed for QC
+
+        -reference: fixed channel image
+        -moving: moving channel image
+        -reference_seg: fixed channel segmentation
+        -moving_seg: moving channel segmentation
+        -ref_mov_coor_dict: dictionary matching reference and moving bead coordinates prior to alignment formated as {(y1,x1): (y2,x2)}
+        -tform: calculated similarity transform for images
+        """
         self.reference = reference
         self.moving_source = moving
 
@@ -40,7 +49,6 @@ class AlignmentQC:
         self.moving_seg = moving_seg
 
         self.ref_mov_coor_dict = ref_mov_coor_dict
-        self.rev_coor_dict = rev_coor_dict
 
         if moving is not None and tform is not None:
             self.moving_transformed = tf.warp(moving, inverse_map=tform, order=3)
@@ -59,13 +67,17 @@ class AlignmentQC:
     def report_full_metrics(
         self,
     ) -> Dict[str, Optional[object]]:
+        """
+        Report all programmed metrics. Requires all variables to be defined
+        """
 
         missing = self.check_all_defined()
         if missing is not None:
-            log.error(
-                "Error: the following variables have not been set - "
-                + ",".join(missing)
+            msg = "Error: the following variables have not been set - " + ",".join(
+                missing
             )
+            log.error(msg)
+            raise ValueError(msg)
 
         z_offset, ref_origin, mov_origin = self.check_z_offset_between_ref_mov()
         ref_signal, ref_noise, mov_signal, mov_noise = self.report_ref_mov_image_snr()
@@ -124,13 +136,6 @@ class AlignmentQC:
         self.ref_mov_coor_dict = ref_mov_coor_dict
         return
 
-    def set_rev_coor_dict(
-        self,
-        rev_coor_dict: Dict[Tuple[int, int], Tuple[int, int]],
-    ):
-        self.rev_coor_dict = rev_coor_dict
-        return
-
     def set_tform(
         self,
         tform: tf.SimilarityTransform,
@@ -158,11 +163,14 @@ class AlignmentQC:
     def apply_transform(self):
         if self.tform is not None:
             self.moving_transformed = tf.warp(
-                self.moving_source[self.mov_origin], inverse_map=self.tform, order=3
-            )
+                self.moving_source[self.mov_origin],
+                inverse_map=self.tform,
+                order=3,
+                preserve_range=True,
+            ).astype(np.uint16)
             self.moving_seg_transformed = tf.warp(
                 self.moving_seg, inverse_map=self.tform, order=0, preserve_range=True
-            )
+            ).astype(np.uint16)
         else:
             log.error("Error: tform is not yet defined")
 
@@ -179,8 +187,6 @@ class AlignmentQC:
             missing.append("moving seg")
         if self.ref_mov_coor_dict is None:
             missing.append("reference moving coordinate dict")
-        if self.rev_coor_dict is None:
-            missing.append("ref coordinate dict")
         if self.moving_transformed is None:
             missing.append("moving transformed")
         if self.moving_seg_transformed is None:
@@ -196,15 +202,25 @@ class AlignmentQC:
     # QC Functions ############################################################
 
     def check_z_offset_between_ref_mov(self) -> Tuple[int, int, int]:
+        """
+        Calculate the difference in between z-midpoints for bead arrays
+        """
+
         if self.reference is None or self.moving_source is None:
             log.error("Error: Raw images are missing for qc")
             raise Exception("Error: Raw images are missing for qc")
+
+        # TODO: Make test with handbuilt numpy arrays
 
         z_offset = self.ref_origin - self.mov_origin
 
         return z_offset, self.ref_origin, self.mov_origin
 
     def report_ref_mov_image_snr(self) -> Tuple[int, int, int, int]:
+        """
+        Calculate signal and noise of reference and moving images
+        """
+
         if self.reference is None or self.moving_source is None:
             log.error("Error: Seg images are missing for qc")
             raise Exception("Error: Seg images are missing for qc")
@@ -214,7 +230,7 @@ class AlignmentQC:
             img_intensity: Optional[NDArray[np.uint16]],
         ) -> Tuple[int, int]:
             if seg is None or img_intensity is None:
-                return -1, -1
+                raise ValueError("Segmentation and raw image must be defined")
             signal = np.median(img_intensity[seg.astype(bool)])
             noise = np.median(img_intensity[~seg.astype(bool)])
 
@@ -230,6 +246,10 @@ class AlignmentQC:
         return ref_signal, ref_noise, mov_signal, mov_noise
 
     def report_number_beads(self) -> Tuple[bool, int]:
+        """
+        Report number of beads matched and whether it's greater than 10
+        """
+
         if self.ref_mov_coor_dict is None:
             log.error("Error: ref_mov_coor_dict is missing for qc")
             raise Exception("Error: ref_mov_coor_dict is missing for qc")
@@ -250,22 +270,24 @@ class AlignmentQC:
             1_percentile: first percentile intensity
             995th_percentile: 99.5th percentile intensity
         """
+
         if self.moving_transformed is None or self.moving_source is None:
             log.error("Error: moving source and transformed images are missing for qc")
             raise Exception(
                 "Error: moving source and transformed images are missing for qc"
             )
 
+        # TODO think of how to test this
         change_fov_intensity_param_dict = {
-            "median_intensity": np.median(self.moving_transformed) * 65535
+            "median_intensity": np.median(self.moving_transformed)
             - np.median(self.moving_source[self.mov_origin]),
-            "min_intensity": np.min(self.moving_transformed) * 65535
+            "min_intensity": np.min(self.moving_transformed)
             - np.min(self.moving_source[self.mov_origin]),
-            "max_intensity": np.max(self.moving_transformed) * 65535
+            "max_intensity": np.max(self.moving_transformed)
             - np.max(self.moving_source[self.mov_origin]),
-            "1st_percentile": np.percentile(self.moving_transformed, 1) * 65535
+            "1st_percentile": np.percentile(self.moving_transformed, 1)
             - np.percentile(self.moving_source[self.mov_origin], 1),
-            "995th_percentile": np.percentile(self.moving_transformed, 99.5) * 65535
+            "995th_percentile": np.percentile(self.moving_transformed, 99.5)
             - np.percentile(self.moving_source[self.mov_origin], 99.5),
         }
 
@@ -307,8 +329,11 @@ class AlignmentQC:
             dist_after_list.append(dist_after)
 
         # filter center beads only
-        y_size = 360
-        x_size = 536
+        # y_size = 360
+        # x_size = 536
+
+        y_size = int(self.reference[self.ref_origin].shape[0] / 3)
+        x_size = int(self.reference[self.ref_origin].shape[1] / 3)
 
         y_lim = (
             int(self.reference[self.ref_origin].shape[0] / 2 - y_size / 2),
@@ -322,8 +347,8 @@ class AlignmentQC:
         dist_before_center = []
         dist_after_center = []
         for bead in range(0, len(mov_coors)):
-            if (y_lim[1] > mov_coors[bead][0]) & (mov_coors[bead][0] > y_lim[0]):
-                if (x_lim[1] > mov_coors[bead][1]) & (mov_coors[bead][1] > x_lim[0]):
+            if (y_lim[1] > mov_coors[bead][0]) and (mov_coors[bead][0] > y_lim[0]):
+                if (x_lim[1] > mov_coors[bead][1]) and (mov_coors[bead][1] > x_lim[0]):
                     dist_before_center.append(
                         distance.euclidean(mov_coors[bead], ref_coors[bead])
                     )
@@ -345,6 +370,7 @@ class AlignmentQC:
             qc: A boolean to indicate if it passed (True) or failed (False) qc
             diff_mse: Difference in mean squared error
         """
+
         qc = False
 
         mse_before = metrics.mean_squared_error(self.reference_seg, self.moving_seg)
