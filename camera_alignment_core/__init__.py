94f95a8769b83d43e6fe7935a9016c6b3156fe96
Gabe Medrash <gabeme@alleninstitute.org>
2022-02-28 12:08:15 -0800

    Bump version: 1.0.0 → 1.0.1

--- camera_alignment_core/__init__.py
+++ camera_alignment_core/__init__.py
@@ -5,7 +5,7 @@ __author__ = "AICS"
 
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 
 def get_module_version():
--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     url="https://github.com/aics-int/camera_alignment_core",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="1.0.0",
+    version="1.0.1",
     zip_safe=False,
 )
